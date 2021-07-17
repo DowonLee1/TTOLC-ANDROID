ae91b5081419bdb5b1e3eb6f3617bb8e8e6afc1d
Dowon <oplolpz@gmail.com>
2021-07-17 16:50:20 -0400

    Button action Added

--- app/src/main/java/com/example/ttolc_android/MainActivity.kt
+++ app/src/main/java/com/example/ttolc_android/MainActivity.kt
@@ -14,8 +14,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val sermonButton = findViewById<Button>(R.id.button15);
+        val buttonA = findViewById<Button>(R.id.button16)
+        val buttonB = findViewById<Button>(R.id.button17)
+
         sermonButton.setOnClickListener {
-            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Fade Out", Toast.LENGTH_SHORT).show()
+
+            sermonButton.animate().apply {
+                buttonB.animate().apply { this.alpha(0.0F) }.start()
+            }.start()
+        }
+
+        buttonA.setOnClickListener {
+            Toast.makeText(this, "Fade In", Toast.LENGTH_SHORT).show()
+
+            buttonA.animate().apply {
+                buttonB.animate().apply { this.alpha(1.0F) }.start()
+            }.start()
         }
 
         val images = listOf<Image>(
